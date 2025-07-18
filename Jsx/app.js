dbff9c34158c13a212670713cf14d33d3f9ece26
Shiva Chandra T <112844747+shivtchandra@users.noreply.github.com>
2025-07-18 16:23:16 +0530

    Update app.js

--- Jsx/app.js
+++ Jsx/app.js
@@ -24,3 +24,32 @@ export default function Board() {
   );
 }
 
+// But here typing button for these many isnt a godd practice as what will we do if we have a long text to be displayed and also huge count of buttons to be displayed 
+// Instead we can use props in the file .
+// so here we define the function of the button and then call it whenevver we want it .
+//Instead of typing the same thiing again and again 
+function Square({ values }) {
+  return <button className="square">{values}</button>;
+}
+
+export default function Board() {
+  return (
+    <>
+      <div className="row">
+        <Square values="1" />
+        <Square values="2" />
+        <Square values="3" />
+      </div>
+      <div className="row">
+        <Square values="4" />
+        <Square values="5" />
+        <Square values="6" />
+      </div>
+      <div className="row">
+        <Square values="7" />
+        <Square values="8" />
+        <Square values="9" />
+      </div>
+    </>
+  );
+}
